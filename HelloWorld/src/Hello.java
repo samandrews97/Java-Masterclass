36931ce9393448e14a8e9d09432c632845b52c8a
samandrews97 <sam@asandrews.co.uk>
2026-06-22 12:46:01 +0100

    Completed Lecture 40

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -23,6 +23,15 @@ public class Hello {
             System.out.println("Either or both of the conditions are true");
         }
 
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is an error");
+        }
+
+        boolean isCar = false;
+        if (!isCar) {
+            System.out.println("This is not supposed to happen");
+        }
     }
 
 }
